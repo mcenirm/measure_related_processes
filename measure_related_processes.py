067a458a1bf064cfadebddcc629f6a186f696144
Michael McEniry <mcenirm@mceniry.net>
2022-01-21 22:03:46 -0600

    minor restyling

--- measure_related_processes.py
+++ measure_related_processes.py
@@ -2,36 +2,33 @@
 
 import argparse
 import csv
-from datetime import datetime, timezone
 import sys
 import time
+from datetime import datetime, timezone
 
 import psutil
 
 
 def build_argument_parser():
     parser = argparse.ArgumentParser(
-        description=(
-            'Run COMMAND and measure the new'
-            ' process and related processes.'
-        ),
+        description="Run COMMAND and measure the new process and related processes."
     )
     parser.add_argument(
-        '-m',
-        dest='measurement_filename',
-        help='name of file to hold measurements',
-        default='measurements.csv',
+        "-m",
+        dest="measurement_filename",
+        help="name of file to hold measurements",
+        default="measurements.csv",
     )
     parser.add_argument(
-        '-s',
-        dest='seconds_between_cycles',
-        help='seconds between cycles',
+        "-s",
+        dest="seconds_between_cycles",
+        help="seconds between cycles",
         default=1.0,
     )
     parser.add_argument(
-        'command',
-        metavar='COMMAND',
-        help='command line to run and measure',
+        "command",
+        metavar="COMMAND",
+        help="command line to run and measure",
         nargs=argparse.REMAINDER,
     )
     return parser
@@ -41,11 +38,11 @@ def parse_arguments():
     parser = build_argument_parser()
     arguments = parser.parse_args()
     if not arguments.command:
-        parser.error('Missing command line')
+        parser.error("Missing command line")
     return arguments
 
 
-class NoOpContextManager():
+class NoOpContextManager:
     def __init__(self):
         pass
 
@@ -56,12 +53,12 @@ class NoOpContextManager():
         return False
 
 
-class SafetyGoggles():
+class SafetyGoggles:
     def __init__(self, backing=None):
         self._backing = backing
 
     def __str__(self):
-        return 'N/A' if self._backing is None else str(self._backing)
+        return "N/A" if self._backing is None else str(self._backing)
 
     def __getattr__(self, name):
         if self._backing is None:
@@ -79,7 +76,7 @@ class SafetyGoggles():
         return result
 
 
-class MeasurementsWriter():
+class MeasurementsWriter:
     def __init__(self, csvfile):
         self.csvfile = csvfile
         self.writer = None
@@ -87,46 +84,46 @@ class MeasurementsWriter():
         self.record = []
 
     def writeprocess(self, cycle, process):
-        if hasattr(process, 'oneshot'):
+        if hasattr(process, "oneshot"):
             oneshot = process.oneshot()
         else:
             oneshot = NoOpContextManager()
         timestamp = time.time()
         with oneshot:
             self.writefield(
-                'Timestamp',
+                "Timestamp",
                 datetime.fromtimestamp(
                     timestamp,
                     timezone.utc,
-                ).isoformat()
+                ).isoformat(),
             )
             p = SafetyGoggles(process)
-            self.writefield('PID', p.pid)
-            self.writefield('PPID', p.ppid)
-            self.writefield('Image', p.name)
-            self.writefield('Elapsed', timestamp - p.create_time)
-            self.writefield('System time', p.cpu_times.system)
-            self.writefield('User time', p.cpu_times.user)
-            self.writefield('Threads', p.num_threads)
-            self.writefield('Active memory', p.memory_info.rss)
-            self.writefield('Virtual memory', p.memory_info.vms)
-            self.writefield('Read operations', p.io_counters.read_count)
-            self.writefield('Read bytes', p.io_counters.read_bytes)
-            self.writefield('Write operations', p.io_counters.write_count)
-            self.writefield('Write bytes', p.io_counters.write_bytes)
-            self.writefield('Cycle', cycle)
-            self.writefield('State', p.status)
-            self.writefield('File descriptors', p.num_fds)
-            self.writefield('Shared memory', p.memory_info.shared)
+            self.writefield("PID", p.pid)
+            self.writefield("PPID", p.ppid)
+            self.writefield("Image", p.name)
+            self.writefield("Elapsed", timestamp - p.create_time)
+            self.writefield("System time", p.cpu_times.system)
+            self.writefield("User time", p.cpu_times.user)
+            self.writefield("Threads", p.num_threads)
+            self.writefield("Active memory", p.memory_info.rss)
+            self.writefield("Virtual memory", p.memory_info.vms)
+            self.writefield("Read operations", p.io_counters.read_count)
+            self.writefield("Read bytes", p.io_counters.read_bytes)
+            self.writefield("Write operations", p.io_counters.write_count)
+            self.writefield("Write bytes", p.io_counters.write_bytes)
+            self.writefield("Cycle", cycle)
+            self.writefield("State", p.status)
+            self.writefield("File descriptors", p.num_fds)
+            self.writefield("Shared memory", p.memory_info.shared)
             self.writefield(
-                'Voluntary context switches',
+                "Voluntary context switches",
                 p.num_ctx_switches.voluntary,
             )
             self.writefield(
-                'Involuntary context switches',
+                "Involuntary context switches",
                 p.num_ctx_switches.involuntary,
             )
-            self.writefield('Current working directory', p.cwd)
+            self.writefield("Current working directory", p.cwd)
             self.writerecord()
 
     def writefield(self, label, value):
@@ -146,7 +143,7 @@ def main():
     arguments = parse_arguments()
     cycle = 0
     first = psutil.Popen(arguments.command)
-    with open(arguments.measurement_filename, 'w') as measurement_f:
+    with open(arguments.measurement_filename, "w") as measurement_f:
         writer = MeasurementsWriter(measurement_f)
         while first.is_running() and first.status() != psutil.STATUS_ZOMBIE:
             cycle += 1
@@ -157,5 +154,5 @@ def main():
         first.wait()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
